4e29b9989086011561d0bad35ff2382e71c6f1f9
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-02 21:51:14 +0300

    add alert dialog

---
+++ lib/common/helper/show_alert_dialog.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:tinder_clone/common/utils/coloors.dart';
+
+showAlertDialog({
+  required BuildContext context,
+  required String message,
+  String? btnText,
+}) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        content: Text(
+          message,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black54,
+            fontSize: 15,
+          ),
+        ),
+        contentPadding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text(
+              btnText ?? "OK",
+              style: TextStyle(
+                color: Coloors.primaryColor,
+              ),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
