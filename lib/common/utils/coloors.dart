d31183ce2e68d55d7fdaac915fad39c54325802e
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-02 21:51:19 +0300

    add utils

---
+++ lib/common/utils/coloors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class Coloors{
+  static const primaryColor = Color.fromRGBO(253, 41, 123, 1.0);
+  static const secondaryHeaderColor = Color.fromRGBO(255, 88, 100, 1.0);
+  static const accentColor = Color.fromRGBO(255, 101, 91, 1.0);
+}
---
+++ lib/common/utils/tinder_icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class TinderIcons {
+  TinderIcons._();
+
+  static const _kFontFam = 'Tinder_clone';
+
+  static const IconData iconfinder_338_tinder_logo_4375488__1_ = IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData iconfinder_message_01_186393 = IconData(0xe801, fontFamily: _kFontFam);
+  static const IconData iconfinder_icons_user2_1564535 = IconData(0xe802, fontFamily: _kFontFam);
+}
