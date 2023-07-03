97c88c5c8ca695dacf1ebc82475674845e1096e6
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-03 20:40:13 +0300

    update theme

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
       onGenerateRoute: Routes.generateRoute,
       home: LoginScreen(),
