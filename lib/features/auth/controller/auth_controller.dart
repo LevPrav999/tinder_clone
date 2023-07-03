2263604212757f96c1df268e5ad7d567d5d12d1b
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-03 20:39:42 +0300

    add controller

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -1,16 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tinder_clone/features/auth/repository/auth_repository.dart';
 
 final authControllerProvider = Provider((ref) {
   final authRepository = ref.watch(authRepositoryProvider);
-  return AuthController(authRepository: authRepository);
+  return AuthController(ref: ref, authRepository: authRepository);
 });
 
 class AuthController {
   final AuthRepository authRepository;
-
-  AuthController({required this.authRepository});
+  final ProviderRef ref;
+  AuthController({ required this.ref, required this.authRepository});
 
   void signInWithPhone(BuildContext context, String phoneNumber) {
     authRepository.signInWithPhone(context, phoneNumber);
@@ -19,5 +21,27 @@ class AuthController {
   void vetifyOTP(BuildContext context, String verificationId, String userCode) {
     authRepository.verifyCode(
         context: context, verificationId: verificationId, smsCode: userCode);
+  }
+
+  void saveDataToFirestore(
+      String name,
+      String age,
+      String sex,
+      String city,
+      String bio,
+      String sexFind,
+      File? profilePicture,
+      BuildContext context) {
+
+    authRepository.saveUserDataToFirebase(
+        name: name,
+        age: age,
+        sex: sex,
+        city: city,
+        bio: bio,
+        sexFind: sexFind,
+        profilePicture: profilePicture,
+        ref: ref,
+        context: context);
   }
 }
