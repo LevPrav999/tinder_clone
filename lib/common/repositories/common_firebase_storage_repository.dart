051131b7c023ec2cb0c8b98ac7f566ccb87cf1d1
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-03 20:39:19 +0300

    add common firebase storage repo

---
+++ lib/common/repositories/common_firebase_storage_repository.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final commonFirebaseStorageRepositoryProvider = Provider((ref) =>
+    CommonFirebaseStorageRepository(firebaseStorage: FirebaseStorage.instance));
+
+class CommonFirebaseStorageRepository {
+  final FirebaseStorage firebaseStorage;
+  CommonFirebaseStorageRepository({required this.firebaseStorage});
+
+  Future<String> storeFileToFirebase(String ref, File file) async {
+    UploadTask uploadTask = firebaseStorage.ref().child(ref).putFile(file);
+
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+
+    return downloadUrl;
+  }
+}
