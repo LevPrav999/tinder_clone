c07802534f5f38b38473c441a9ba7ddae986c659
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-04 18:21:06 +0300

    add loader widget

---
+++ lib/common/widgets/loader.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoaderWidget extends StatelessWidget {
+  const LoaderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
