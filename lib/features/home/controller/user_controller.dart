a8adbe0a0502f38d7133af78bdd3f8f325555906
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-04 18:21:56 +0300

    add user controller

---
+++ lib/features/home/controller/user_controller.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tinder_clone/common/models/user_model.dart';
+import 'package:tinder_clone/features/home/repositories/user_repository.dart';
+
+final userControllerProvider = Provider((ref) {
+  final userRepository = ref.watch(userRepositoryProvider);
+  return UserController(userRepository: userRepository);
+});
+
+final userDataProvider = FutureProvider((ref) {
+  final userController = ref.watch(userControllerProvider);
+  return userController.getUser();
+});
+
+class UserController{
+  final UserRepository userRepository;
+
+  UserController({required this.userRepository});
+
+
+  Future<UserModel> getUser() async{
+    return await userRepository.getUserDataFromFirestore();
+  }
+}
