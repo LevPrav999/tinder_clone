d952574a746063727f1686e1786b1c0b29b4aa43
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-07 18:58:51 +0300

    add cards state with index

--- lib/features/home/controller/cards_controller.dart
+++ lib/features/home/controller/cards_controller.dart
@@ -2,21 +2,37 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tinder_clone/common/widgets/match_card.dart';
 import 'package:tinder_clone/features/home/repositories/cards_repository.dart';
 
-final cardsControllerProvider = StateNotifierProvider<CardsController, List<MatchCard>>((ref) {
+final cardsControllerProvider = StateNotifierProvider<CardsController, CardsState>((ref) {
   final cardsRepository = ref.watch(cardsRepositoryProvider);
   return CardsController(cardsRepository: cardsRepository);
 });
 
+class CardsState{
+  final List<MatchCard> cards;
+  final int index;
 
-class CardsController extends StateNotifier<List<MatchCard>>{
+  CardsState({required this.cards, required this.index});
+
+  CardsState copyWith({List<MatchCard>? cards, int? index}){
+      return CardsState(cards: cards ?? this.cards, index: index ?? this.index);
+  }
+}
+
+class CardsController extends StateNotifier<CardsState>{
   final CardsRepository cardsRepository;
 
-  CardsController({required this.cardsRepository}) : super([]);
+  CardsController({required this.cardsRepository}) : super(CardsState(cards: [], index: 0));
 
-  void setState() async{
-    var data = await cardsRepository.getCards();
-    state = data;
+  void setCards() async{
+    var list = await cardsRepository.getCards();
+
+    state = state.copyWith(cards: list);
   }
+
+  void setIndex(int index){
+    state = state.copyWith(index: index);
+  }
+
 
   void addToLiked(String uidToAdd){
     cardsRepository.addToLiked(uidToAdd);
