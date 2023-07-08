2a2ee2b869af1bc40ed050a9ab210e217f795f71
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-08 18:10:48 +0300

    add state

--- lib/features/matchers/controller/match_controller.dart
+++ lib/features/matchers/controller/match_controller.dart
@@ -1,23 +1,56 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tinder_clone/common/widgets/match_card.dart';
+import 'package:tinder_clone/features/home/screens/home_screen.dart';
 import 'package:tinder_clone/features/matchers/repositories/match_repository.dart';
 
 
-final matchControllerProvider = StateNotifierProvider<MatchController, List<MatchCard>>((ref) {
+final matchControllerProvider = StateNotifierProvider<MatchController, CardsState>((ref) {
   final matchRepository = ref.watch(matchRepositoryProvider);
   return MatchController(matchRepository: matchRepository);
 });
 
+class CardsState{
+  final List<MatchCard> cards;
+  final int index;
 
-class MatchController extends StateNotifier<List<MatchCard>>{
+  CardsState({required this.cards, required this.index});
+
+  CardsState copyWith({List<MatchCard>? cards, int? index}){
+      return CardsState(cards: cards ?? this.cards, index: index ?? this.index);
+  }
+}
+
+class MatchController extends StateNotifier<CardsState>{
   final MatchRepository matchRepository;
 
-  MatchController({required this.matchRepository}) : super([]);
+  MatchController({required this.matchRepository}) : super(CardsState(cards: [], index: 0));
 
 
   void setCards() async{
     var list = await matchRepository.getMatchers();
 
-    state = list;
+    state = state.copyWith(cards: list);
+  }
+
+  void setIndex(int index){
+    state = state.copyWith(index: index);
+  }
+
+  void goBack(BuildContext context) async{
+    Future.delayed(const Duration(seconds: 3)).then((value) => Navigator.pushNamedAndRemoveUntil(context, HomeScreen.routeName, (route) => false));
+  }
+
+  void deletePending(String uidUser) async{
+    matchRepository.deletePendingUser(uidUser);
+  }
+
+
+  void removeFromBlocked(String uidToRemove){
+    matchRepository.removeFromBlocked(uidToRemove);
+  }
+
+  void addToPending(String uidUser) async{
+    matchRepository.addToPending(uidUser);
   }
 }
