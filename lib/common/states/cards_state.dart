3e98876dc51d9dcddb7e567ec3495a4674e5600c
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-09 17:25:01 +0300

    add cards state file

---
+++ lib/common/states/cards_state.dart
@@ -0,0 +1,12 @@
+import 'package:tinder_clone/common/widgets/match_card.dart';
+
+class CardsState{
+  final List<MatchCard> cards;
+  final int index;
+
+  CardsState({required this.cards, required this.index});
+
+  CardsState copyWith({List<MatchCard>? cards, int? index}){
+      return CardsState(cards: cards ?? this.cards, index: index ?? this.index);
+  }
+}
