54779a7179f6e526d0ddc320b0f36fe38b0795bd
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-09 21:02:00 +0300

    add getAllLastMessageList func

--- lib/features/chat/controller/chat_controller.dart
+++ lib/features/chat/controller/chat_controller.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tinder_clone/common/models/user_model.dart';
+import 'package:tinder_clone/common/models/chat_model.dart';
+import 'package:tinder_clone/features/auth/controller/auth_controller.dart';
 import 'package:tinder_clone/features/chat/repositories/chat_repository.dart';
-import 'package:tinder_clone/features/home/controller/user_controller.dart';
 
 final chatControllerProvider = Provider((ref) {
   final chatRepository = ref.watch(chatRepositoryProvider);
@@ -18,13 +18,19 @@ class ChatController {
     required this.ref,
   });
 
+  Stream<List<ChatConversation>> getAllLastMessageList() {
+    return chatRepository.getAllLastMessageList();
+  }
+
   void sendTextMessage(
-      BuildContext context, String text, UserModel receiverUser) {
-    ref.read(userDataProvider).whenData((user) =>
-        chatRepository.sendTextMessage(
+      BuildContext context, String text, String receiverUserId) async{
+    
+    final user = await ref.read(userInfoAuthProvider.future);
+    chatRepository.sendTextMessage(
             context: context,
             text: text,
-            recieverUserId: receiverUser.uid,
-            senderUser: user));
+            recieverUserId: receiverUserId,
+            senderUser: user!);
+
   }
 }
