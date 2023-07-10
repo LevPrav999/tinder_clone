84cf9a49960a7223c84480e9efef4c4f97000640
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-10 19:21:51 +0300

    add message field decoration

--- lib/common/utils/decorations.dart
+++ lib/common/utils/decorations.dart
@@ -21,4 +21,23 @@ class Decorations{
       borderRadius: BorderRadius.all(Radius.circular(30.0)),
     ),
   );
+
+  static const kTextFieldMessageDecoration = InputDecoration(
+    hintText: 'Message',
+    hintStyle: TextStyle(fontSize: 16, color: Color(0xFF737373)),
+    contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+    fillColor: Color(0xFFF1F2F6),
+    filled: true,
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Color(0xFFF1F2F6), width: 1.0),
+      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: Color(0xFFF1F2F6), width: 2.0),
+      borderRadius: BorderRadius.all(Radius.circular(30.0)),
+    ),
+  );
 }
