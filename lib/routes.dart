1bb7eef429fa0e07c42ed2e6df8d02a1371d5c45
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-10 19:26:11 +0300

    add chat screen route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:tinder_clone/common/models/user_model.dart';
 import 'package:tinder_clone/common/widgets/error.dart';
 import 'package:tinder_clone/features/auth/screens/phone_auth_screen.dart';
 import 'package:tinder_clone/features/auth/screens/code_screen.dart';
 import 'package:tinder_clone/features/auth/screens/user_information_screen.dart';
+import 'package:tinder_clone/features/chat/screens/chat_screen.dart';
 import 'package:tinder_clone/features/home/screens/home_screen.dart';
 import 'package:tinder_clone/features/login/screens/login_screen.dart';
 import 'package:tinder_clone/features/matchers/screens/match_screen.dart';
@@ -18,6 +20,9 @@ class Routes {
         return MaterialPageRoute(builder: (context) => const MatchScreen(), maintainState: false);
       case HomeScreen.routeName:
         return MaterialPageRoute(builder: (context) => const HomeScreen(), maintainState: false);
+      case ChatScreen.routeName:
+        final user = settings.arguments as UserModel;
+        return MaterialPageRoute(builder: (context) => ChatScreen(user: user));
       case UserInfoScreen.routeName:
         final data = settings.arguments as Map<String, dynamic>?;
         return MaterialPageRoute(builder: (context) => UserInfoScreen(
