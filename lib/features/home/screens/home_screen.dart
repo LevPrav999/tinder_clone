d207893de95bbfcd00cc09c3fa2caca22c024d47
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-10 19:25:28 +0300

    ConsumerWidget to ConsumerStatefulWidget

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -3,17 +3,53 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tinder_clone/common/utils/coloors.dart';
 import 'package:tinder_clone/common/utils/tinder_icons.dart';
+import 'package:tinder_clone/features/auth/controller/auth_controller.dart';
 import 'package:tinder_clone/features/home/controller/home_controller.dart';
 import 'package:tinder_clone/features/home/screens/cards_tab_screen.dart';
 import 'package:tinder_clone/features/home/screens/chats_tab_screen.dart';
 import 'package:tinder_clone/features/home/screens/profile_tab_screen.dart';
 
-class HomeScreen extends ConsumerWidget {
-  static const String routeName = '/home-screen';
+
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
+  static const String routeName = '/home-screen';
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> with WidgetsBindingObserver{
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    switch(state){
+      case AppLifecycleState.resumed:
+        ref.read(authControllerProvider).setUserStatus(true);
+        break;
+      case AppLifecycleState.inactive:
+      case AppLifecycleState.detached:
+      case AppLifecycleState.paused:
+        ref.read(authControllerProvider).setUserStatus(false);
+        break;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     var index = ref.watch(homeControllerProvider);
 
     return DefaultTabController(
