bbbc02a57ada1f7028f8ab5fc59467bc3f5f4254
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-10 19:22:03 +0300

    add lastSeenMessage util

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -40,3 +40,20 @@ String getAge(int birthYear) {
   
   return age.toString();
 }
+
+String lastSeenMessage(lastSeen) {
+  DateTime now = DateTime.now();
+  Duration differenceDuration = now.difference(
+    DateTime.fromMillisecondsSinceEpoch(lastSeen),
+  );
+
+  String finalMessage = differenceDuration.inSeconds > 59
+      ? differenceDuration.inMinutes > 59
+          ? differenceDuration.inHours > 23
+              ? "${differenceDuration.inDays} ${differenceDuration.inDays == 1 ? 'day' : 'days'}"
+              : "${differenceDuration.inHours} ${differenceDuration.inHours == 1 ? 'hour' : 'hours'}"
+          : "${differenceDuration.inMinutes} ${differenceDuration.inMinutes == 1 ? 'minute' : 'minutes'}"
+      : 'few moments';
+
+  return finalMessage;
+}
