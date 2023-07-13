242d3f0b6e213835654d76751ca63565fdb637b5
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-13 20:51:49 +0300

    add arrayContainsAny to getCards func

--- lib/features/home/repositories/cards_repository.dart
+++ lib/features/home/repositories/cards_repository.dart
@@ -6,6 +6,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tinder_clone/common/models/user_model.dart';
 import 'package:tinder_clone/common/widgets/match_card.dart';
 
+import '../../../common/utils/utils.dart';
+
 final cardsRepositoryProvider = Provider((ref) => CardsRepository(
     auth: FirebaseAuth.instance, firestore: FirebaseFirestore.instance));
 
@@ -17,49 +19,64 @@ class CardsRepository{
   CardsRepository({required this.auth, required this.firestore});
 
 
-  Future<List<MatchCard>> getCards() async{
-    String uid = auth.currentUser!.uid;
-    var userData = await firestore.collection('users').doc(uid).get();
-    UserModel user = UserModel.fromMap(userData.data()!);
+  Future<List<MatchCard>> getCards() async {
+  String uid = auth.currentUser!.uid;
+  var userData = await firestore.collection('users').doc(uid).get();
+  UserModel user = UserModel.fromMap(userData.data()!);
 
-    final minBirthDate = DateTime(
-        user.age['year'] - 1, user.age['month'], user.age['day']);
-    final maxBirthDate = DateTime(
-        user.age['year'] + 1, user.age['month'], user.age['day']);
+  final minBirthDate = DateTime(
+    user.age['year'] - 1, user.age['month'], user.age['day']);
+  final maxBirthDate = DateTime(
+    user.age['year'] + 1, user.age['month'], user.age['day']);
 
-    final querySnapshot = await firestore
-        .collection('users')
-        .where('city', isEqualTo: user.city)
-        .where('sex', isEqualTo: user.sexFind)
-        .where('sexFind', isEqualTo: user.sex)
-        .where('age.year', isGreaterThanOrEqualTo: minBirthDate.year)
-        .where('age.year', isLessThanOrEqualTo: maxBirthDate.year)
-        .get();
+  CollectionReference usersRef = firestore.collection('users');
+  Query query = usersRef
+      .where('city', isEqualTo: user.city)
+      .where('sex', isEqualTo: user.sexFind)
+      .where('sexFind', isEqualTo: user.sex)
+      .where('age.year', isGreaterThanOrEqualTo: minBirthDate.year)
+      .where('age.year', isLessThanOrEqualTo: maxBirthDate.year);
+
+  List<List<String>> subsets = splitListIntoSubsets(user.tags, 10);
+  
+  List<Future<QuerySnapshot>> queryFutures = [];
+
+  for (List<String> subset in subsets) {
+    Query subsetQuery = query.where('tags', arrayContainsAny: subset);
+    queryFutures.add(subsetQuery.get());
+  }
+
+  List<QuerySnapshot> querySnapshots = await Future.wait(queryFutures);
+
+  List<MatchCard> randomDocuments = [];
+
+  for (QuerySnapshot querySnapshot in querySnapshots) {
+    List<QueryDocumentSnapshot> documents = querySnapshot.docs;
+    documents.removeWhere((doc) => doc.id == uid);
 
     final random = Random();
-    final documents = querySnapshot.docs;
-    final randomDocuments = <MatchCard>[];
-
-    documents.removeWhere((doc) => doc.id == uid);
 
     for (var i = 0; i < 10; i++) {
       if (documents.isEmpty) break;
       final randomIndex = random.nextInt(documents.length);
       final document = documents[randomIndex];
       if (user.liked.contains(document.id) ||
-          user.blocked.contains(document.id) || user.pending.contains(document.id)) {
+          user.blocked.contains(document.id) ||
+          user.pending.contains(document.id)) {
         documents.removeAt(randomIndex);
         continue;
       }
       final matchCard = MatchCard(
-        user: UserModel.fromMap(document.data())
+        user: UserModel.fromMap(document.data() as Map<String, dynamic>),
       );
       randomDocuments.add(matchCard);
       documents.removeAt(randomIndex);
     }
-
-    return randomDocuments;
   }
+
+  return randomDocuments;
+}
+
 
   void addToLiked(String uidToAdd) async {
     String uid = auth.currentUser!.uid;
