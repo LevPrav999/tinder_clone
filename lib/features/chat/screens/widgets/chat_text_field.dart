874a3a960a5e89acbf34acf9c97a622eb8de974d
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-21 18:35:34 +0300

    add scroll to text field

--- lib/features/chat/screens/widgets/chat_text_field.dart
+++ lib/features/chat/screens/widgets/chat_text_field.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tinder_clone/common/helper/show_alert_dialog.dart';
 import 'package:tinder_clone/common/models/user_model.dart';
@@ -10,7 +9,8 @@ import 'package:tinder_clone/common/utils/decorations.dart';
 import 'package:tinder_clone/features/chat/controller/chat_controller.dart';
 
 class ChatTextField extends ConsumerStatefulWidget {
-  const ChatTextField({super.key, required this.receiverUser, required this.messageController});
+  const ChatTextField(
+      {super.key, required this.receiverUser, required this.messageController});
   final UserModel receiverUser;
   final ScrollController messageController;
 
@@ -19,7 +19,6 @@ class ChatTextField extends ConsumerStatefulWidget {
 }
 
 class _ChatTextFieldState extends ConsumerState<ChatTextField> {
-
   late TextEditingController textEditingController;
   late final ScrollController messageController;
 
@@ -29,19 +28,24 @@ class _ChatTextFieldState extends ConsumerState<ChatTextField> {
     messageController = widget.messageController;
     textEditingController = TextEditingController();
   }
+
   @override
   void dispose() {
     super.dispose();
     textEditingController.dispose();
   }
 
-  void sendMessage() async{
-    if(textEditingController.text.trim().isEmpty){
+  void sendMessage() async {
+    if (textEditingController.text.trim().isEmpty) {
       showAlertDialog(context: context, message: "your_message_is_empty".tr());
-    }else{
+    } else {
       messageController.jumpTo(messageController.position.maxScrollExtent);
-      await MessagingApi().callOnFcmApiSendPushNotifications(widget.receiverUser.fcmToken, "new_message".tr(), textEditingController.text);
-      ref.read(chatControllerProvider).sendTextMessage(context, textEditingController.text, widget.receiverUser.uid);
+      await MessagingApi().callOnFcmApiSendPushNotifications(
+          widget.receiverUser.fcmToken,
+          "new_message".tr(),
+          textEditingController.text);
+      ref.read(chatControllerProvider).sendTextMessage(
+          context, textEditingController.text, widget.receiverUser.uid);
       textEditingController.clear();
     }
   }
@@ -49,25 +53,25 @@ class _ChatTextFieldState extends ConsumerState<ChatTextField> {
   @override
   Widget build(BuildContext context) {
     return Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Stack(
-                alignment: Alignment.centerRight,
-                children: [
-                  TextField(
-                    controller: textEditingController,
-                    decoration: Decorations.kTextFieldMessageDecoration
-                        .copyWith(hintText: "message".tr()),
-                    style: TextStyle(color: Colors.black, fontSize: 16),
-                  ),
-                  IconButton(
-                          color: Coloors.primaryColor,
-                          icon: Icon(Icons.send),
-                          onPressed: () => sendMessage(),
-                        ),
-                ],
-              )),
-            );
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Stack(
+            alignment: Alignment.centerRight,
+            children: [
+              TextField(
+                controller: textEditingController,
+                decoration: Decorations.kTextFieldMessageDecoration
+                    .copyWith(hintText: "message".tr()),
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+              IconButton(
+                color: Coloors.primaryColor,
+                icon: Icon(Icons.send),
+                onPressed: () => sendMessage(),
+              ),
+            ],
+          )),
+    );
   }
 }
