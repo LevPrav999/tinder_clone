ea7893542198e0ff5fea0c86490a4386f0f86c92
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-21 19:10:25 +0300

    fix button

--- lib/features/chat/screens/widgets/chat_scroll_button.dart
+++ lib/features/chat/screens/widgets/chat_scroll_button.dart
@@ -11,10 +11,9 @@ class ChatScrollButton extends StatefulWidget {
 }
 
 class _ChatScrollButtonState extends State<ChatScrollButton> {
-
   late final ScrollController messageController;
 
-  bool showScrollButton = true;
+  bool showScrollButton = false;
 
   @override
   void initState() {
@@ -29,32 +28,40 @@ class _ChatScrollButtonState extends State<ChatScrollButton> {
     super.dispose();
   }
 
-
   void _scrollListener() {
-    setState(() {
-      showScrollButton = messageController.position.pixels <
-          MediaQuery.of(context).size.height / 2;
-    });
+    if (messageController.position.maxScrollExtent.toInt() < 350) {
+      setState(() {
+        showScrollButton = false;
+      });
+    } else {
+      setState(() {
+        showScrollButton = messageController.position.pixels <
+            MediaQuery.of(context).size.height / 2;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return showScrollButton ? Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 78.0),
-                  child: FloatingActionButton(
-                      onPressed: () {
-                        messageController.animateTo(
-                          messageController.position.maxScrollExtent,
-                          duration: const Duration(milliseconds: 300),
-                          curve: Curves.easeInOut,
-                        );
-                      },
-                      backgroundColor: Coloors.skyBlue,
-                      child: const Icon(Icons.arrow_downward),
-                    ),
-                ),
-                ) : Container();
+
+    return showScrollButton
+        ? Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 78.0),
+              child: FloatingActionButton(
+                onPressed: () {
+                  messageController.animateTo(
+                    messageController.position.maxScrollExtent,
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                },
+                backgroundColor: Coloors.skyBlue,
+                child: const Icon(Icons.arrow_downward),
+              ),
+            ),
+          )
+        : Container();
   }
 }
