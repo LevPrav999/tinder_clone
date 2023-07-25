2f07cdd89bc09489bd39c299c604a5b0b627d0f2
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-25 22:40:38 +0300

    add secret.dart

---
+++ lib/secret.dart
@@ -0,0 +1,3 @@
+class Secrets{
+  static const String serverKey = 'AAAANyy3jGQ:APA91bFnfcN9zcwd23izvx7RGtxF3PuMphNT5g6ANxNAuzU4FhkKP10RmkwLJqw1oeoYbCU3SQXa0hkS9V4dbHz6rNxpezFROtg4RMudqdczcyuwA2ZTSDSavwQy1UJg6Tuow9Tr33TA';
+}
