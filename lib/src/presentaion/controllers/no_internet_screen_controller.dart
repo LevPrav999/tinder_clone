d6dd4b21b30c12d5c7d82c23ef863e3935950fbe
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-26 12:41:16 +0300

    delete no internet controller

--- lib/src/presentaion/controllers/no_internet_screen_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-
-final connectivityResultProvider = StreamProvider<ConnectivityResult>(
-  (ref) => Connectivity().onConnectivityChanged,
-);
