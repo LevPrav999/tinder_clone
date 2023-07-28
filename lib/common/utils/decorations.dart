6d84f1309c16cd6b4839f5968d6f4a3c11d084cf
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-28 17:00:18 +0300

    add consts

--- lib/common/utils/decorations.dart
+++ lib/common/utils/decorations.dart
@@ -4,19 +4,19 @@ import 'package:flutter/material.dart';
 class Decorations{
   static var kTextFieldDecoration = InputDecoration(
     hintText: 'enter_here'.tr(),
-    hintStyle: TextStyle(fontSize: 22),
-    contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-    fillColor: Color.fromARGB(255, 230, 232, 243),
+    hintStyle: const TextStyle(fontSize: 22),
+    contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+    fillColor: const Color.fromARGB(255, 230, 232, 243),
     filled: true,
-    border: OutlineInputBorder(
+    border: const OutlineInputBorder(
       borderRadius: BorderRadius.all(Radius.circular(30.0)),
     ),
-    enabledBorder: OutlineInputBorder(
+    enabledBorder: const OutlineInputBorder(
       borderSide:
           BorderSide(color: Color.fromARGB(255, 230, 232, 243), width: 1.0),
       borderRadius: BorderRadius.all(Radius.circular(30.0)),
     ),
-    focusedBorder: OutlineInputBorder(
+    focusedBorder: const OutlineInputBorder(
       borderSide:
           BorderSide(color: Color.fromARGB(255, 230, 232, 243), width: 2.0),
       borderRadius: BorderRadius.all(Radius.circular(30.0)),
@@ -25,18 +25,18 @@ class Decorations{
 
   static var kTextFieldMessageDecoration = InputDecoration(
     hintText: 'message'.tr(),
-    hintStyle: TextStyle(fontSize: 16, color: Color(0xFF737373)),
-    contentPadding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-    fillColor: Color(0xFFF1F2F6),
+    hintStyle: const TextStyle(fontSize: 16, color: Color(0xFF737373)),
+    contentPadding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+    fillColor: const Color(0xFFF1F2F6),
     filled: true,
-    border: OutlineInputBorder(
+    border: const OutlineInputBorder(
       borderRadius: BorderRadius.all(Radius.circular(30.0)),
     ),
-    enabledBorder: OutlineInputBorder(
+    enabledBorder: const OutlineInputBorder(
       borderSide: BorderSide(color: Color(0xFFF1F2F6), width: 1.0),
       borderRadius: BorderRadius.all(Radius.circular(30.0)),
     ),
-    focusedBorder: OutlineInputBorder(
+    focusedBorder: const OutlineInputBorder(
       borderSide: BorderSide(color: Color(0xFFF1F2F6), width: 2.0),
       borderRadius: BorderRadius.all(Radius.circular(30.0)),
     ),
