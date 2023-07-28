1edd92471269e812ac1ebf598ae66ea5e46f683d
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-28 16:36:22 +0300

    add connection message state provider

--- lib/src/presentaion/states/user_state.dart
+++ lib/src/presentaion/states/user_state.dart
@@ -7,3 +7,5 @@ import '../../domain/user_model.dart';
 final userStateProvider = StateProvider<UserModel?>((ref) => null);
 
 final userStatusStateProvider = StateProvider<bool?>((ref) => null);
+
+final connectionStateProvider = StateProvider<bool?>((ref) => false);
