31b980c854642124a3ee02ad40ace77cc6609cb6
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-28 17:00:12 +0300

    add overrides

--- lib/common/errors/errors.dart
+++ lib/common/errors/errors.dart
@@ -7,24 +7,28 @@ abstract class Failture implements Exception{
 
 class NotAutomaticRetrieved implements Failture{
   final String verificationId;
+  @override
   final String message;
 
   NotAutomaticRetrieved(this.verificationId, this.message);
 }
 
 class ErrorLoginPhone implements Failture{
+  @override
   final String message;
 
   ErrorLoginPhone(this.message);
 }
 
 class FirestoreError implements Failture{
+  @override
   final String message;
 
   FirestoreError(this.message);
 }
 
 class TagsUnselected implements Failture{
+  @override
   final String message;
 
   TagsUnselected(this.message);
